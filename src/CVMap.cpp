b5cc4aec558131a1d9bc1cc6e9866d213dad1f32
ben2012 <ben@kristall-klang.de>
2019-06-10 15:24:07 +0200

    moved duplicate code to template MapModule

--- src/CVMap.cpp
+++ src/CVMap.cpp
@@ -1,11 +1,12 @@
 #include "plugin.hpp"
 #include "widgets.hpp"
+#include "MapModule.hpp"
 #include <chrono>
 
 static const int MAX_CHANNELS = 32;
 static const float UINIT = 0;
 
-struct CV_Map : Module {
+struct CV_Map : MapModule<MAX_CHANNELS> {
 	enum ParamIds {
 		NUM_PARAMS
 	};
@@ -22,19 +23,6 @@ struct CV_Map : Module {
 		ENUMS(CHANNEL_LIGHTS2, 16),
 		NUM_LIGHTS
 	};
-
-	/** Number of maps */
-	int mapLen = 0;
-	/** The mapped param handle of each channel */
-	ParamHandle paramHandles[MAX_CHANNELS];
-
-	/** Channel ID of the learning session */
-	int learningId;
-	/** Whether the param has been set during the learning session */
-	bool learnedParam;
-
-	/** The smoothing processor (normalized between 0 and 1) of each channel */
-	dsp::ExponentialFilter valueFilters[MAX_CHANNELS];
 
 	bool bipolarInput = false;
 
@@ -60,13 +48,6 @@ struct CV_Map : Module {
 		for (int id = 0; id < MAX_CHANNELS; id++) {
 			APP->engine->removeParamHandle(&paramHandles[id]);
 		}
-	}
-
-	void onReset() override {
-		learningId = -1;
-		learnedParam = false;
-		clearMaps();
-		mapLen = 1;
 	}
 
 	void process(const ProcessArgs &args) override {
@@ -127,83 +108,8 @@ struct CV_Map : Module {
 		}
 	}
 
-	void clearMap(int id) {
-		learningId = -1;
-		APP->engine->updateParamHandle(&paramHandles[id], -1, 0, true);
-		valueFilters[id].reset();
-		lastValue[id] = UINIT;
-		updateMapLen();
-	}
-
-	void clearMaps() {
-		learningId = -1;
-		for (int id = 0; id < MAX_CHANNELS; id++) {
-			APP->engine->updateParamHandle(&paramHandles[id], -1, 0, true);
-			valueFilters[id].reset();
-			lastValue[id] = UINIT;
-		}
-		mapLen = 0;
-	}
-
-	void updateMapLen() {
-		// Find last nonempty map
-		int id;
-		for (id = MAX_CHANNELS - 1; id >= 0; id--) {
-			if (paramHandles[id].moduleId >= 0)
-				break;
-		}
-		mapLen = id + 1;
-		// Add an empty "Mapping..." slot
-		if (mapLen < MAX_CHANNELS)
-			mapLen++;
-	}
-
-	void commitLearn() {
-		if (learningId < 0)
-			return;
-		if (!learnedParam)
-			return;
-		// Reset learned state
-		learnedParam = false;
-		// Find next incomplete map
-		while (++learningId < MAX_CHANNELS) {
-			if (paramHandles[learningId].moduleId < 0)
-				return;
-		}
-		learningId = -1;
-	}
-
-	void enableLearn(int id) {
-		if (learningId != id) {
-			learningId = id;
-			learnedParam = false;
-		}
-	}
-
-	void disableLearn(int id) {
-		if (learningId == id) {
-			learningId = -1;
-		}
-	}
-
-	void learnParam(int id, int moduleId, int paramId) {
-		APP->engine->updateParamHandle(&paramHandles[id], moduleId, paramId, true);
-		learnedParam = true;
-		commitLearn();
-		updateMapLen();
-	}
-
 	json_t *dataToJson() override {
-		json_t *rootJ = json_object();
-
-		json_t *mapsJ = json_array();
-		for (int id = 0; id < mapLen; id++) {
-			json_t *mapJ = json_object();
-			json_object_set_new(mapJ, "moduleId", json_integer(paramHandles[id].moduleId));
-			json_object_set_new(mapJ, "paramId", json_integer(paramHandles[id].paramId));
-			json_array_append(mapsJ, mapJ);
-		}
-		json_object_set_new(rootJ, "maps", mapsJ);
+		json_t *rootJ = MapModule::dataToJson();
 
 		json_t *lockParameterChangesJ = json_boolean(lockParameterChanges);
 		json_object_set_new(rootJ, "lockParameterChanges", lockParameterChangesJ);
@@ -215,211 +121,13 @@ struct CV_Map : Module {
 	}
 
 	void dataFromJson(json_t *rootJ) override {
-		clearMaps();
-
-		json_t *mapsJ = json_object_get(rootJ, "maps");
-		if (mapsJ) {
-			json_t *mapJ;
-			size_t mapIndex;
-			json_array_foreach(mapsJ, mapIndex, mapJ) {
-				json_t *moduleIdJ = json_object_get(mapJ, "moduleId");
-				json_t *paramIdJ = json_object_get(mapJ, "paramId");
-				if (!(moduleIdJ && paramIdJ))
-					continue;
-				if (mapIndex >= MAX_CHANNELS)
-					continue;
-				APP->engine->updateParamHandle(&paramHandles[mapIndex], json_integer_value(moduleIdJ), json_integer_value(paramIdJ), false);
-			}
-		}
-		updateMapLen();
+		MapModule::dataFromJson(rootJ);
 
 		json_t *lockParameterChangesJ = json_object_get(rootJ, "lockParameterChanges");
 		lockParameterChanges = json_boolean_value(lockParameterChangesJ);
 
 		json_t *bipolarInputJ = json_object_get(rootJ, "bipolarInput");
 		bipolarInput = json_boolean_value(bipolarInputJ);
-	}
-};
-
-
-struct CV_MapChoice : LedDisplayChoice {
-	CV_Map *module;
-	int id;
-
-	static const int hscrollCharMAXLENGTH = 16;
-	std::chrono::time_point<std::chrono::system_clock> hscrollUpdate = std::chrono::system_clock::now();
-	int hscrollCharOffset = 0;
-
-	CV_MapChoice() {
-		box.size = mm2px(Vec(0, 7.5));
-		textOffset = Vec(6, 14.7);
-	}
-
-	void setModule(CV_Map *module) {
-		this->module = module;
-	}
-
-	void onButton(const event::Button &e) override {
-		e.stopPropagating();
-		if (!module)
-			return;
-
-		if (e.action == GLFW_PRESS && e.button == GLFW_MOUSE_BUTTON_LEFT) {
-			e.consume(this);
-		}
-
-		if (e.action == GLFW_PRESS && e.button == GLFW_MOUSE_BUTTON_RIGHT) {
-			module->clearMap(id);
-			e.consume(this);
-		}
-	}
-
-	void onSelect(const event::Select &e) override {
-		if (!module)
-			return;
-
-		ScrollWidget *scroll = getAncestorOfType<ScrollWidget>();
-		scroll->scrollTo(box);
-
-		// Reset touchedParam
-		APP->scene->rack->touchedParam = NULL;
-		module->enableLearn(id);
-	}
-
-	void onDeselect(const event::Deselect &e) override {
-		if (!module)
-			return;
-		// Check if a ParamWidget was touched
-		ParamWidget *touchedParam = APP->scene->rack->touchedParam;
-		if (touchedParam) {
-			APP->scene->rack->touchedParam = NULL;
-			int moduleId = touchedParam->paramQuantity->module->id;
-			int paramId = touchedParam->paramQuantity->paramId;
-			module->learnParam(id, moduleId, paramId);
-			hscrollCharOffset = 0;
-		}
-		else {
-			module->disableLearn(id);
-		}
-	}
-
-	void step() override {
-		if (!module)
-			return;
-
-		// Set bgColor and selected state
-		if (module->learningId == id) {
-			bgColor = color;
-			bgColor.a = 0.15;
-
-			// HACK
-			if (APP->event->selectedWidget != this)
-				APP->event->setSelected(this);
-		} else {
-			bgColor = nvgRGBA(0, 0, 0, 0);
-
-			// HACK
-			if (APP->event->selectedWidget == this)
-				APP->event->setSelected(NULL);
-		}
-
-		// Set text
-		text = string::f("%02d ", id + 1);
-		if (module->paramHandles[id].moduleId >= 0 && module->learningId != id) {
-			std::string pn = getParamName();
-			if (pn.length() > hscrollCharMAXLENGTH) {
-				// Scroll the parameter-name horizontically
-				text += pn.substr(hscrollCharOffset > (int)pn.length() ? 0 : hscrollCharOffset);
-				auto now = std::chrono::system_clock::now();
-				if (now - hscrollUpdate > std::chrono::milliseconds{100}) {
-					hscrollCharOffset = (hscrollCharOffset + 1) % (pn.length() + hscrollCharMAXLENGTH);
-					hscrollUpdate = now;
-				}
-			} else {
-				text += pn;
-			}
-		} else  {
-			if (module->learningId == id) {
-				text += "Mapping...";
-			} else {
-				text += "Unmapped";
-			}
-		}
-
-		// Set text color
-		if (module->paramHandles[id].moduleId >= 0 || module->learningId == id) {
-			color.a = 1.0;
-		} else {
-			color.a = 0.5;
-		}
-	}
-
-	std::string getParamName() {
-		if (!module)
-			return "";
-		if (id >= module->mapLen)
-			return "<ERROR>";
-		ParamHandle *paramHandle = &module->paramHandles[id];
-		if (paramHandle->moduleId < 0)
-			return "<ERROR>";
-		ModuleWidget *mw = APP->scene->rack->getModule(paramHandle->moduleId);
-		if (!mw)
-			return "<ERROR>";
-		// Get the Module from the ModuleWidget instead of the ParamHandle.
-		// I think this is more elegant since this method is called in the app world instead of the engine world.
-		Module *m = mw->module;
-		if (!m)
-			return "<ERROR>";
-		int paramId = paramHandle->paramId;
-		if (paramId >= (int) m->params.size())
-			return "<ERROR>";
-		ParamQuantity *paramQuantity = m->paramQuantities[paramId];
-		std::string s;
-		s += mw->model->name;
-		s += " ";
-		s += paramQuantity->label;
-		return s;
-	}
-};
-
-
-struct CV_MapDisplay : LedDisplay {
-	CV_Map *module;
-	ScrollWidget *scroll;
-	CV_MapChoice *choices[MAX_CHANNELS];
-	LedDisplaySeparator *separators[MAX_CHANNELS];
-
-	void setModule(CV_Map *module) {
-		this->module = module;
-
-		scroll = new ScrollWidget;
-		scroll->box.size.x = box.size.x;
-		scroll->box.size.y = box.size.y - scroll->box.pos.y;
-		addChild(scroll);
-
-		LedDisplaySeparator *separator = createWidget<LedDisplaySeparator>(scroll->box.pos);
-		separator->box.size.x = box.size.x;
-		addChild(separator);
-		separators[0] = separator;
-
-		Vec pos;
-		for (int id = 0; id < MAX_CHANNELS; id++) {
-			if (id > 0) {
-				LedDisplaySeparator *separator = createWidget<LedDisplaySeparator>(pos);
-				separator->box.size.x = box.size.x;
-				scroll->container->addChild(separator);
-				separators[id] = separator;
-			}
-
-			CV_MapChoice *choice = createWidget<CV_MapChoice>(pos);
-			choice->box.size.x = box.size.x;
-			choice->id = id;
-			choice->setModule(module);
-			scroll->container->addChild(choice);
-			choices[id] = choice;
-
-			pos = choice->box.getBottomLeft();
-		}
 	}
 };
 
@@ -448,7 +156,7 @@ struct CV_MapWidget : ModuleWidget {
 		w1->setModule(module, CV_Map::CHANNEL_LIGHTS2);
 		addChild(w1);
 
-		CV_MapDisplay *mapWidget = createWidget<CV_MapDisplay>(mm2px(Vec(3.41891, 29.f)));
+		MapModuleDisplay<MAX_CHANNELS> *mapWidget = createWidget<MapModuleDisplay<MAX_CHANNELS>>(mm2px(Vec(3.41891, 29.f)));
 		mapWidget->box.size = mm2px(Vec(43.999, 91));
 		mapWidget->setModule(module);
 		addChild(mapWidget);
