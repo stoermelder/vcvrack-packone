535c6704cc891b42e1ea85ab33e21fa54399e9d5
ben2012 <ben@kristall-klang.de>
2019-06-20 16:38:33 +0200

    azure pipelines

--- scripts/release-notes.sh
+++ scripts/release-notes.sh
@@ -3,18 +3,17 @@
 cat <<- EOH
 ## Automatically generated PackOne build
 
-This build is automatically generated every time I push to v1. As such it is the latest version of the code and may be unstable, unusable, unsuitable for human consumption, and so on.
+This build is automatically generated every time I push my changes. As such it is the latest version of the code and may be unstable, unusable, unsuitable for human consumption, and so on.
 
-The latetest stable build can be found at the [VCV Library](https://vcvrack.com/plugins.html#packone).
+The latest stable build can be found at the [VCV Library](https://vcvrack.com/plugins.html#packone).
 
 These assets were built against
 https://vcvrack.com/downloads/Rack-SDK-1.0.0.zip
 
 The build date and most recent commits are:
-
 EOH
 date
 echo ""
 echo "Most recent commits:" 
 echo ""
-git log --pretty=oneline | head -5
+git log --pretty=oneline | head -3
