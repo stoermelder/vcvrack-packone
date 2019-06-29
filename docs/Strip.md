3e2520917035fdc8cf8691e6b4b8a7d3fe1a6889
stoermelder <ben@kristall-klang.de>
2019-06-29 14:22:27 +0200

    Strip - implemented experimental save/load

--- docs/Strip.md
+++ docs/Strip.md
@@ -2,6 +2,6 @@
 
 (Added in v1.0.4) A module for disabling or enabling a group of modules using CV. A group of modules is formed in a single row without a space between them, directly attached left or right to STRIP. The module uses Rack v1's expander-mechanism to traverse the affected modules.
 
-The ON-section can be configured to enable only (Default) or to toggle On/Off through the context menu.
+The ON-section can be configured through the context menu to enable only (Default) or to toggle On/Off.
 
 ![STRIP Intro](./Strip-intro.gif)
--- src/Strip.cpp
+++ src/Strip.cpp
@@ -1,4 +1,6 @@
 #include "plugin.hpp"
+#include <osdialog.h>
+#include <plugin.hpp>
 #include <thread>
 
 
@@ -24,8 +26,8 @@ struct Strip : Module {
 	};
 
     /** [Stored to JSON] */
-
     int onMode = STRIP_ONMODE_DEFAULT;
+    
     bool lastState = false;
 
 	dsp::SchmittTrigger onTrigger;
@@ -61,6 +63,10 @@ struct Strip : Module {
         }
     }
 
+	void saveGroup() {
+
+	}
+
     json_t *dataToJson() override {
 		json_t *rootJ = json_object();
 		json_object_set_new(rootJ, "onMode", json_boolean(onMode));
@@ -74,6 +80,7 @@ struct Strip : Module {
 	}
 };
 
+static const char PRESET_FILTERS[] = "stoermelder STRIP group preset (.vcvss):vcvss";
 
 struct StripWidget : ModuleWidget {
 	StripWidget(Strip *module) {
@@ -89,7 +96,254 @@ struct StripWidget : ModuleWidget {
         addParam(createParamCentered<TL1105>(Vec(22.5f, 149.4f), module, Strip::OFF_PARAM));
 	}
 
-	
+	ModuleWidget *moduleFromJson(json_t *moduleJ) {
+		// Get slugs
+		json_t *pluginSlugJ = json_object_get(moduleJ, "plugin");
+		if (!pluginSlugJ)
+			return NULL;
+		json_t *modelSlugJ = json_object_get(moduleJ, "model");
+		if (!modelSlugJ)
+			return NULL;
+		std::string pluginSlug = json_string_value(pluginSlugJ);
+		std::string modelSlug = json_string_value(modelSlugJ);
+
+		// Get Model
+		plugin::Model *model = plugin::getModel(pluginSlug, modelSlug);
+		if (!model)
+			return NULL;
+
+		// Create ModuleWidget
+		ModuleWidget *moduleWidget = model->createModuleWidget();
+		assert(moduleWidget);
+		moduleWidget->fromJson(moduleJ);
+		return moduleWidget;
+	}
+
+	ModuleWidget *addModuleToRack(json_t *moduleJ, Rect &box, bool left) {
+		ModuleWidget *moduleWidget = moduleFromJson(moduleJ);
+		if (moduleWidget) {
+			moduleWidget->box.pos = left ? box.pos.minus(Vec(moduleWidget->box.size.x, 0)) : box.pos;
+			APP->scene->rack->addModule(moduleWidget);
+			APP->scene->rack->setModulePosForce(moduleWidget, moduleWidget->box.pos);
+			box.size = moduleWidget->box.size;
+			box.pos = moduleWidget->box.pos;
+			return moduleWidget;
+		}
+		else {
+			json_t *pluginSlugJ = json_object_get(moduleJ, "plugin");
+			json_t *modelSlugJ = json_object_get(moduleJ, "model");
+			std::string pluginSlug = json_string_value(pluginSlugJ);
+			std::string modelSlug = json_string_value(modelSlugJ);
+			//APP->patch->warningLog += string::f("Could not find module \"%s\" of plugin \"%s\"\n", modelSlug.c_str(), pluginSlug.c_str());
+			box = Rect(box.pos, Vec(0, 0));
+			return NULL;
+		}
+	}
+
+	void loadGroup(std::string filename) {
+		INFO("Loading preset %s", filename.c_str());
+
+		FILE *file = fopen(filename.c_str(), "r");
+		if (!file) {
+			WARN("Could not load patch file %s", filename.c_str());
+			return;
+		}
+		DEFER({
+			fclose(file);
+		});
+
+		json_error_t error;
+		json_t *rootJ = json_loadf(file, 0, &error);
+		if (!rootJ) {
+			std::string message = string::f("File is not a valid patch file. JSON parsing error at %s %d:%d %s", error.source, error.line, error.column, error.text);
+			osdialog_message(OSDIALOG_WARNING, OSDIALOG_OK, message.c_str());
+			return;
+		}
+		DEFER({
+			json_decref(rootJ);
+		});
+
+		// remove modules directly attached to STRIP
+		std::vector<int> toBeRemoved;
+        Module *m = module;
+        while (m) {
+            if (m->rightExpander.moduleId < 0) break;
+			toBeRemoved.push_back(m->rightExpander.moduleId);
+            m = m->rightExpander.module;
+        }
+		m = module;
+        while (m) {
+            if (m->leftExpander.moduleId < 0) break;
+			toBeRemoved.push_back(m->leftExpander.moduleId);
+            m = m->leftExpander.module;
+        }
+		for (std::vector<int>::iterator it = toBeRemoved.begin() ; it != toBeRemoved.end(); ++it) {
+			ModuleWidget *mw = APP->scene->rack->getModule(*it);
+			APP->scene->rack->removeModule(mw);
+		}
+
+		// add modules
+		std::map<int, ModuleWidget*> modules;
+		int mc = 0;
+		Rect box = this->box;
+		json_t *rightModulesJ = json_object_get(rootJ, "rightModules");
+		if (rightModulesJ) {
+			json_t *moduleJ;
+			size_t moduleIndex;
+			json_array_foreach(rightModulesJ, moduleIndex, moduleJ) {
+				box.pos = box.pos.plus(Vec(box.size.x, 0));
+				ModuleWidget *mw = addModuleToRack(moduleJ, box, false);
+				modules[mc++] = mw;
+			}
+		}
+		box = this->box;
+		json_t *leftModulesJ = json_object_get(rootJ, "leftModules");
+		if (leftModulesJ) {
+			json_t *moduleJ;
+			size_t moduleIndex;
+			json_array_foreach(leftModulesJ, moduleIndex, moduleJ) {
+				ModuleWidget *mw = addModuleToRack(moduleJ, box, true);
+				modules[mc++] = mw;
+			}
+		}
+
+		// add cables
+		json_t *cablesJ = json_object_get(rootJ, "cables");
+		if (cablesJ) {
+			json_t *cableJ;
+			size_t cableIndex;
+			json_array_foreach(cablesJ, cableIndex, cableJ) {
+				int outputIndex, outputPortId, inputIndex, inputPortId;
+				json_unpack(cableJ, "[i, i, i, i]", &outputIndex, &outputPortId, &inputIndex, &inputPortId);
+
+				ModuleWidget *mw1 = modules[outputIndex];
+				ModuleWidget *mw2 = modules[inputIndex];
+				PortWidget *pw1 = mw1->outputs[outputPortId];
+				PortWidget *pw2 = mw2->inputs[inputPortId];
+
+				CableWidget *cw = new CableWidget;
+				cw->setOutput(pw1);
+				cw->setInput(pw2);
+				APP->scene->rack->addCable(cw);
+			}
+		}
+	}
+
+	void saveGroup(std::string filename) {
+		INFO("Saving preset %s", filename.c_str());
+
+		// modules
+		std::map<ModuleWidget*, int> modules;
+		int mc = 0;
+		json_t *rightModulesJ = json_array();
+        Module *m = module;
+        while (m) {
+            if (m->rightExpander.moduleId < 0) break;
+			ModuleWidget *mw = APP->scene->rack->getModule(m->rightExpander.moduleId);
+			json_t *moduleJ = mw->toJson();
+			assert(moduleJ);
+			json_array_append_new(rightModulesJ, moduleJ);
+			modules[mw] = mc++;
+            m = m->rightExpander.module;
+        }
+
+		json_t *leftModulesJ = json_array();
+        m = module;
+        while (m) {
+            if (m->leftExpander.moduleId < 0) break;
+			ModuleWidget *mw = APP->scene->rack->getModule(m->leftExpander.moduleId);
+			json_t *moduleJ = mw->toJson();
+			assert(moduleJ);
+			json_array_append_new(leftModulesJ, moduleJ);
+			modules[mw] = mc++;
+            m = m->leftExpander.module;
+        }
+
+		// cables
+		json_t *cablesJ = json_array();
+		for (auto it1 = modules.begin(); it1 != modules.end(); ++it1) {
+			int outputIndex = it1->second;
+			// it is enough to check the inputs, as output with other end outside the group doesn't matter
+			for (PortWidget* output : it1->first->outputs) {
+				for (CableWidget *cw : APP->scene->rack->getCablesOnPort(output)) {
+					if (!cw->isComplete())
+						continue;
+
+					PortWidget* input = cw->inputPort;
+					ModuleWidget *mw2 = APP->scene->rack->getModule(input->module->id);
+					auto it2 = modules.find(mw2);
+					if (it2 == modules.end())
+						continue;
+
+					int inputIndex = it2->second;
+					json_t *cableJ = json_pack("[i, i, i, i]", outputIndex, output->portId, inputIndex, input->portId);
+					json_array_append_new(cablesJ, cableJ);
+				}
+			}
+		}
+
+		json_t *rootJ = json_object();
+		json_object_set_new(rootJ, "rightModules", rightModulesJ);
+		json_object_set_new(rootJ, "leftModules", leftModulesJ);
+		json_object_set_new(rootJ, "cables", cablesJ);
+
+		DEFER({
+			json_decref(rootJ);
+		});
+
+		FILE *file = fopen(filename.c_str(), "w");
+		if (!file) {
+			WARN("Could not write to patch file %s", filename.c_str());
+		}
+		DEFER({
+			fclose(file);
+		});
+
+		json_dumpf(rootJ, file, JSON_INDENT(2) | JSON_REAL_PRECISION(9));
+	}
+
+	void loadGroupDialog() {
+		osdialog_filters *filters = osdialog_filters_parse(PRESET_FILTERS);
+		DEFER({
+			osdialog_filters_free(filters);
+		});
+
+		char *path = osdialog_file(OSDIALOG_OPEN, "", NULL, filters);
+		if (!path) {
+			// No path selected
+			return;
+		}
+		DEFER({
+			free(path);
+		});
+
+		loadGroup(path);
+	}
+
+	void saveGroupDialog() {
+		osdialog_filters *filters = osdialog_filters_parse(PRESET_FILTERS);
+		DEFER({
+			osdialog_filters_free(filters);
+		});
+
+		char *path = osdialog_file(OSDIALOG_SAVE, "", "Untitled.vcvss", filters);
+		if (!path) {
+			// No path selected
+			return;
+		}
+		DEFER({
+			free(path);
+		});
+
+		std::string pathStr = path;
+		std::string extension = string::filenameExtension(string::filename(pathStr));
+		if (extension.empty()) {
+			pathStr += ".vcvss";
+		}
+
+		saveGroup(pathStr);
+	}
+
 	void appendContextMenu(Menu *menu) override {
 		Strip *module = dynamic_cast<Strip*>(this->module);
 		assert(module);
@@ -119,6 +373,28 @@ struct StripWidget : ModuleWidget {
 
         OnModeMenuItem *onModeMenuItem = construct<OnModeMenuItem>(&MenuItem::text, "ON mode", &OnModeMenuItem::module, module);
         menu->addChild(onModeMenuItem);
+		menu->addChild(new MenuSeparator());
+
+		struct LoadGroupMenuItem : MenuItem {
+			StripWidget *moduleWidget;
+
+			void onAction(const event::Action &e) override {
+				moduleWidget->loadGroupDialog();
+			}
+		};
+
+		struct SaveGroupMenuItem : MenuItem {
+			StripWidget *moduleWidget;
+
+			void onAction(const event::Action &e) override {
+				moduleWidget->saveGroupDialog();
+			}
+		};
+
+		LoadGroupMenuItem *loadGroupMenuItem = construct<LoadGroupMenuItem>(&MenuItem::text, "Load group of modules", &LoadGroupMenuItem::moduleWidget, this);
+		menu->addChild(loadGroupMenuItem);
+        SaveGroupMenuItem *saveGroupMenuItem = construct<SaveGroupMenuItem>(&MenuItem::text, "Save group of modules", &SaveGroupMenuItem::moduleWidget, this);
+		menu->addChild(saveGroupMenuItem);
   	}
 };
 
