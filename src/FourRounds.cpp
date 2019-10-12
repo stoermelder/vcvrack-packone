fc6cb0224842954b5793c26b66eb9318590cff2c
stoermelder <ben@kristall-klang.de>
2019-10-12 16:07:10 +0200

    4Rounds - panel wip

--- src/FourRounds.cpp
+++ src/FourRounds.cpp
@@ -111,7 +111,7 @@ struct FourRoundsWidget : ModuleWidget {
 		addChild(createWidget<MyBlackScrew>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<MyBlackScrew>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addInput(createInputCentered<PJ301MPort>(Vec(150.0f, 242.8f), module,  FourRoundsModule::TRIG_INPUT));
+		addInput(createInputCentered<PJ301MPort>(Vec(150.0f, 241.3f), module,  FourRoundsModule::TRIG_INPUT));
 
 		addInput(createInputCentered<PJ301MPort>(Vec(175.6f, 65.7f), module,  FourRoundsModule::ROUND1_INPUT + 0));
 		addInput(createInputCentered<PJ301MPort>(Vec(222.9f, 85.3f), module,  FourRoundsModule::ROUND1_INPUT + 1));
@@ -147,7 +147,7 @@ struct FourRoundsWidget : ModuleWidget {
 		addOutput(createOutputCentered<PJ301MPort>(Vec(178.2f, 194.3f), module, FourRoundsModule::ROUND4_OUTPUT + 0));
 		addOutput(createOutputCentered<PJ301MPort>(Vec(121.8f, 194.3f), module, FourRoundsModule::ROUND4_OUTPUT + 1));
 
-		addOutput(createOutputCentered<PJ301MPort>(Vec(150.0f, 168.0f), module, FourRoundsModule::WINNER_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(Vec(150.0f, 159.0f), module, FourRoundsModule::WINNER_OUTPUT));
 
 		addChild(createLightCentered<TinyLight<GreenLight>>(Vec(178.6f, 83.9f), module,  FourRoundsModule::ROUND_LIGHT + 0));
 		addChild(createLightCentered<TinyLight<GreenLight>>(Vec(207.8f, 96.2f), module,  FourRoundsModule::ROUND_LIGHT + 1));
