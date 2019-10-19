c13f528bf961a4c012d134a643f095293cc422aa
stoermelder <ben@kristall-klang.de>
2019-10-19 09:48:05 +0200

    Sipo - renamed "offset" to "skip"

--- src/Sipo.cpp
+++ src/Sipo.cpp
@@ -9,14 +9,14 @@ static const int MAX_DATA_32_16 = MAX_DATA_32 / PORT_MAX_CHANNELS;
 
 struct SipoModule : Module {
 	enum ParamIds {
-		OFFSET_PARAM,
+		SKIP_PARAM,
 		INCR_PARAM,
 		NUM_PARAMS
 	};
 	enum InputIds {
 		SRC_INPUT,
 		TRIG_INPUT,
-		OFFSET_INPUT,
+		SKIP_INPUT,
 		INCR_INPUT,
 		NUM_INPUTS
 	};
@@ -39,7 +39,7 @@ struct SipoModule : Module {
 
 	SipoModule() {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-		configParam(OFFSET_PARAM, 1.f, MAX_DATA_32, 1.f, "Trigger-count to the current value, 1 acts as a standard shift register");
+		configParam(SKIP_PARAM, 0.f, MAX_DATA_32 - 1, 0.f, "Trigger-skip to the current value, 0 acts as a standard shift register");
 		configParam(INCR_PARAM, 0.f, MAX_DATA_32_16, 0.f, "Inrement between used cells, 0 acts as standard shift register");
 		data = new float[MAX_DATA];
 
@@ -68,21 +68,21 @@ struct SipoModule : Module {
 			data[dataPtr] = inputs[SRC_INPUT].getVoltage();
 		}
 
-		int offsetCv = std::round(rescale(inputs[OFFSET_INPUT].getVoltage(), 0.f, 10.f, 0, MAX_DATA_32));
-		int offset = (int) clamp((int)params[OFFSET_PARAM].getValue() + offsetCv, 1, MAX_DATA_32);
+		int skipCv = std::round(rescale(inputs[SKIP_INPUT].getVoltage(), 0.f, 10.f, 0, MAX_DATA_32 - 1));
+		int skip = 1 + (int)clamp((int)params[SKIP_PARAM].getValue() + skipCv, 0, MAX_DATA_32 - 1);
 		int incrCv = std::round(rescale(inputs[INCR_INPUT].getVoltage(), 0.f, 10.f, 0, MAX_DATA_32_16));
-		int incr = (int) clamp((int)params[INCR_PARAM].getValue() + incrCv, 0, MAX_DATA_32_16);
+		int incr = (int)clamp((int)params[INCR_PARAM].getValue() + incrCv, 0, MAX_DATA_32_16);
 
 		for (int c = 0; c < PORT_MAX_CHANNELS; c++) {
-			outputs[POLY_OUTPUT].setVoltage(data[(dataPtr - (offset + incr * c) * c + MAX_DATA) % MAX_DATA], c);
+			outputs[POLY_OUTPUT].setVoltage(data[(dataPtr - (skip + incr * c) * c + MAX_DATA) % MAX_DATA], c);
 		}
 
 		// Set channel lights infrequently
 		if (lightDivider.process()) {
 			for (int c = 0; c < PORT_MAX_CHANNELS; c++) {
 				float f = outputs[POLY_OUTPUT].getVoltage(c);
-				lights[CHANNEL_LIGHTS + 2 * c + 0].setBrightness(f < 0.f ? 0 : rescale(f, 0.f, 5.f, 0.f, 1.f));
-				lights[CHANNEL_LIGHTS + 2 * c + 1].setBrightness(f > 0.f ? 0 : rescale(f, -5.f, 0.f, 0.f, 1.f));
+				lights[CHANNEL_LIGHTS + 2 * c + 0].setBrightness(f <= 0.f ? 0 : rescale(f, 0.f, 5.f, 0.f, 1.f));
+				lights[CHANNEL_LIGHTS + 2 * c + 1].setBrightness(f >= 0.f ? 0 : rescale(f, -5.f, 0.f, 0.f, 1.f));
 			}
 		}
 	}
@@ -127,8 +127,8 @@ struct SipoWidget : ModuleWidget {
 
 		addInput(createInputCentered<StoermelderPort>(Vec(22.5f, 83.4f), module, SipoModule::TRIG_INPUT));
 
-		addInput(createInputCentered<StoermelderPort>(Vec(22.5f, 126.9f), module, SipoModule::OFFSET_INPUT));
-		StoermelderTrimpot* tp1 = createParamCentered<StoermelderTrimpot>(Vec(22.5f, 151.5f), module, SipoModule::OFFSET_PARAM);
+		addInput(createInputCentered<StoermelderPort>(Vec(22.5f, 126.9f), module, SipoModule::SKIP_INPUT));
+		StoermelderTrimpot* tp1 = createParamCentered<StoermelderTrimpot>(Vec(22.5f, 151.5f), module, SipoModule::SKIP_PARAM);
 		tp1->snap = true;
 		addParam(tp1);
 
