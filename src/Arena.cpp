67ff398573fbbd9bfccbcc100c7f27ee0649a3ea
stoermelder <ben@kristall-klang.de>
2019-10-20 19:02:03 +0200

    Arena - wip

--- src/Arena.cpp
+++ src/Arena.cpp
@@ -5,12 +5,30 @@ namespace Arena {
 
 struct ArenaModule : Module {
 	enum ParamIds {
+		ENUMS(IN_X_PARAM, 8),
+		ENUMS(IN_Y_PARAM, 8),
+		ENUMS(IN_CTRL_PARAM, 8),
+		ENUMS(IN_BCTRL_PARAM, 8),
+		ENUMS(IN_PLUS_PARAM, 8),
+		ENUMS(IN_MINUS_PARAM, 8),
+		ALL_CTRL_PARAM,
+		ALL_BCTRL_PARAM,
+		ALL_PLUS_PARAM,
+		ALL_MINUS_PARAM,
 		NUM_PARAMS
 	};
 	enum InputIds {
+		ENUMS(INPUT, 8),
+		ENUMS(IN_X_INPUT, 8),
+		ENUMS(IN_Y_INPUT, 8),
+		ENUMS(CTRL_INPUT, 8),
+		ENUMS(OUT_X_INPUT, 2),
+		ENUMS(OUT_Y_INPUT, 2),
+		ALL_CTRL_INPUT,
 		NUM_INPUTS
 	};
 	enum OutputIds {
+		ENUMS(OUTPUT, 2),
 		NUM_OUTPUTS
 	};
 	enum LightIds {
