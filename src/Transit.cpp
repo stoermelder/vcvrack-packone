be312386f57cd90a7e5801945c2bb4f8da60aa4e
stoermelder <ben@kristall-klang.de>
2020-07-14 23:00:52 +0200

    Transit - wip #146

--- src/Transit.cpp
+++ src/Transit.cpp
@@ -19,22 +19,24 @@ enum SLOTCVMODE {
 template < int NUM_PRESETS >
 struct TransitModule : Module {
 	enum ParamIds {
-		MODE_PARAM,
-		ENUMS(PRESET_PARAM, NUM_PRESETS),
+		PARAM_RW,
 		PARAM_FADE,
 		PARAM_SHAPE,
+		ENUMS(PARAM_PRESET, NUM_PRESETS),
 		NUM_PARAMS
 	};
 	enum InputIds {
-		SLOT_INPUT,
-		RESET_INPUT,
+		INPUT_SLOT,
+		INPUT_RESET,
+		INPUT_FADE,
+		INPUT_SHAPE,
 		NUM_INPUTS
 	};
 	enum OutputIds {
 		NUM_OUTPUTS
 	};
 	enum LightIds {
-		ENUMS(PRESET_LIGHT, NUM_PRESETS * 3),
+		ENUMS(LIGHT_PRESET, NUM_PRESETS * 3),
 		NUM_LIGHTS
 	};
 
@@ -42,24 +44,15 @@ struct TransitModule : Module {
 	int panelTheme = 0;
 
 	/** [Stored to JSON] */
-	std::string sourcePluginSlug;
-	/** [Stored to JSON] */
-	std::string sourcePluginName;
-	/** [Stored to JSON] */
-	std::string sourceModelSlug;
-	/** [Stored to JSON] */
-	std::string sourceModelName;
-
-	/** [Stored to JSON] */
 	bool presetSlotUsed[NUM_PRESETS];
 	/** [Stored to JSON] */
 	std::vector<float> presetSlot[NUM_PRESETS];
 	/** [Stored to JSON] */
-	int preset = 0;
+	int preset;
 	/** [Stored to JSON] */
-	int presetCount = NUM_PRESETS;
+	int presetCount;
 
-	int presetNext = -1;
+	int presetNext;
 
 	std::vector<float> presetOld;
 
@@ -71,9 +64,8 @@ struct TransitModule : Module {
 	bool mappingIndicatorHidden = false;
 
 	std::default_random_engine randGen{(uint16_t)std::chrono::system_clock::now().time_since_epoch().count()};
-	std::uniform_int_distribution<int>* randDist = NULL;
+	std::uniform_int_distribution<int> randDist;
 	bool inChange = false;
-	float modeLight = 0;
 
 	/** [Stored to JSON] */
 	std::vector<ParamHandle*> sourceHandles;
@@ -91,10 +83,10 @@ struct TransitModule : Module {
 	TransitModule() {
 		panelTheme = pluginSettings.panelThemeDefault;
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-		configParam(MODE_PARAM, 0, 1, 0, "Switch Read/write mode");
+		configParam(PARAM_RW, 0, 1, 0, "Switch Read/write mode");
 		for (int i = 0; i < NUM_PRESETS; i++) {
-			configParam(PRESET_PARAM + i, 0, 1, 0, string::f("Preset slot %d", i + 1));
-			typeButtons[i].param = &params[PRESET_PARAM + i];
+			configParam<TriggerParamQuantity>(PARAM_PRESET + i, 0, 1, 0, string::f("Preset slot %d", i + 1));
+			typeButtons[i].param = &params[PARAM_PRESET + i];
 			presetSlotUsed[i] = false;
 		}
 		configParam(PARAM_FADE, 0.f, 1.f, 0.5f, "Fade time");
@@ -111,7 +103,6 @@ struct TransitModule : Module {
 			APP->engine->removeParamHandle(sourceHandle);
 			delete sourceHandle;
 		}
-		delete randDist;
 	}
 
 	void onReset() override {
@@ -123,11 +114,6 @@ struct TransitModule : Module {
 		sourceHandles.clear();
 		inChange = false;
 
-		sourcePluginSlug = "";
-		sourcePluginName = "";
-		sourceModelSlug = "";
-		sourceModelName = "";
-
 		for (int i = 0; i < NUM_PRESETS; i++) {
 			presetSlotUsed[i] = false;
 			presetSlot[i].clear();
@@ -136,10 +122,11 @@ struct TransitModule : Module {
 		preset = -1;
 		presetCount = NUM_PRESETS;
 		presetNext = -1;
-        slewLimiter.out = 1.f;
+		slewLimiter.out = 1.f;
 
-		if (randDist) delete randDist;
-		randDist = new std::uniform_int_distribution<int>(0, presetCount - 1);
+		randDist = std::uniform_int_distribution<int>(0, presetCount - 1);
+		mappingIndicatorHidden = false;
+		Module::onReset();
 	}
 
 	void process(const ProcessArgs& args) override {
@@ -153,34 +140,34 @@ struct TransitModule : Module {
 		}
 
 		// Read mode
-		if (params[MODE_PARAM].getValue() == 0.f) {
+		if (params[PARAM_RW].getValue() == 0.f) {
 			// RESET input
 			if (slotCvMode == SLOTCVMODE_TRIG_FWD || slotCvMode == SLOTCVMODE_TRIG_REV || slotCvMode == SLOTCVMODE_TRIG_PINGPONG) {
-				if (inputs[RESET_INPUT].isConnected() && resetTrigger.process(inputs[RESET_INPUT].getVoltage())) {
+				if (inputs[INPUT_RESET].isConnected() && resetTrigger.process(inputs[INPUT_RESET].getVoltage())) {
 					resetTimer.reset();
 					presetLoad(0);
 				}
 			}
 
 			// SLOT input
-			if (resetTimer.process(args.sampleTime) >= 1e-3f && inputs[SLOT_INPUT].isConnected()) {
+			if (resetTimer.process(args.sampleTime) >= 1e-3f && inputs[INPUT_SLOT].isConnected()) {
 				switch (slotCvMode) {
 					case SLOTCVMODE_10V:
-						presetLoad(std::floor(rescale(inputs[SLOT_INPUT].getVoltage(), 0.f, 10.f, 0, presetCount)));
+						presetLoad(std::floor(rescale(inputs[INPUT_SLOT].getVoltage(), 0.f, 10.f, 0, presetCount)));
 						break;
 					case SLOTCVMODE_C4:
-						presetLoad(std::round(clamp(inputs[SLOT_INPUT].getVoltage() * 12.f, 0.f, NUM_PRESETS - 1.f)));
+						presetLoad(std::round(clamp(inputs[INPUT_SLOT].getVoltage() * 12.f, 0.f, NUM_PRESETS - 1.f)));
 						break;
 					case SLOTCVMODE_TRIG_FWD:
-						if (slotTrigger.process(inputs[SLOT_INPUT].getVoltage()))
+						if (slotTrigger.process(inputs[INPUT_SLOT].getVoltage()))
 							presetLoad((preset + 1) % presetCount);
 						break;
 					case SLOTCVMODE_TRIG_REV:
-						if (slotTrigger.process(inputs[SLOT_INPUT].getVoltage()))
+						if (slotTrigger.process(inputs[INPUT_SLOT].getVoltage()))
 							presetLoad((preset - 1 + presetCount) % presetCount);
 						break;
 					case SLOTCVMODE_TRIG_PINGPONG:
-						if (slotTrigger.process(inputs[SLOT_INPUT].getVoltage())) {
+						if (slotTrigger.process(inputs[INPUT_SLOT].getVoltage())) {
 							int n = preset + slotCvModeDir;
 							if (n == presetCount - 1) 
 								slotCvModeDir = -1;
@@ -190,11 +177,11 @@ struct TransitModule : Module {
 						}
 						break;
 					case SLOTCVMODE_TRIG_RANDOM:
-						if (slotTrigger.process(inputs[SLOT_INPUT].getVoltage()))
-							presetLoad((*randDist)(randGen));
+						if (slotTrigger.process(inputs[INPUT_SLOT].getVoltage()))
+							presetLoad(randDist(randGen));
 						break;
 					case SLOTCVMODE_ARM:
-						if (slotTrigger.process(inputs[SLOT_INPUT].getVoltage()))
+						if (slotTrigger.process(inputs[INPUT_SLOT].getVoltage()))
 							presetLoad(presetNext);
 						break;
 				}
@@ -239,19 +226,17 @@ struct TransitModule : Module {
 		// Set channel lights infrequently
 		if (lightDivider.process()) {
 			float s = args.sampleTime * lightDivider.getDivision();
-			modeLight += 0.7f * s;
-			if (modeLight > 1.5f) modeLight = 0.f;
 
 			for (int i = 0; i < NUM_PRESETS; i++) {
-				if (params[MODE_PARAM].getValue() == 0.f) {
-					lights[PRESET_LIGHT + i * 3 + 0].setBrightness(presetNext == i ? 1.f : 0.f);
-					lights[PRESET_LIGHT + i * 3 + 1].setSmoothBrightness(preset != i && presetCount > i ? (presetSlotUsed[i] ? 1.f : 0.2f) : 0.f, s);
-					lights[PRESET_LIGHT + i * 3 + 2].setSmoothBrightness(preset == i ? 1.f : 0.f, s);
+				if (params[PARAM_RW].getValue() == 0.f) {
+					lights[LIGHT_PRESET + i * 3 + 0].setBrightness(presetNext == i ? 1.f : 0.f);
+					lights[LIGHT_PRESET + i * 3 + 1].setSmoothBrightness(preset != i && presetCount > i ? (presetSlotUsed[i] ? 1.f : 0.2f) : 0.f, s);
+					lights[LIGHT_PRESET + i * 3 + 2].setSmoothBrightness(preset == i ? 1.f : 0.f, s);
 				}
 				else {
-					lights[PRESET_LIGHT + i * 3 + 0].setBrightness(presetSlotUsed[i] ? 1.f : 0.f);
-					lights[PRESET_LIGHT + i * 3 + 1].setBrightness(0.f);
-					lights[PRESET_LIGHT + i * 3 + 2].setBrightness(0.f);
+					lights[LIGHT_PRESET + i * 3 + 0].setBrightness(presetSlotUsed[i] ? 1.f : 0.f);
+					lights[LIGHT_PRESET + i * 3 + 1].setBrightness(0.f);
+					lights[LIGHT_PRESET + i * 3 + 2].setBrightness(0.f);
 				}
 			}
 		}
@@ -274,18 +259,12 @@ struct TransitModule : Module {
 		return paramQuantity;
 	}
 
-	void bindToSource() {
+	void bindModule() {
 		Expander* exp = &leftExpander;
 		if (exp->moduleId < 0) return;
 
 		inChange = true;
-		onReset();
 		Module* m = exp->module;
-		sourcePluginSlug = m->model->plugin->slug;
-		sourcePluginName = m->model->plugin->name;
-		sourceModelSlug = m->model->slug;
-		sourceModelName = m->model->name;
-
 		for (size_t i = 0; i < m->params.size(); i++) {
 			ParamHandle* sourceHandle = new ParamHandle;
 			sourceHandle->text = "stoermelder TRANSIT";
@@ -293,7 +272,6 @@ struct TransitModule : Module {
 			APP->engine->updateParamHandle(sourceHandle, m->id, i, true);
 			sourceHandles.push_back(sourceHandle);
 		}
-
 		inChange = false;
 	}
 
@@ -322,15 +300,19 @@ struct TransitModule : Module {
 
 	void presetProcess(float sampleTime) {
 		if (preset == -1) return;
-		slewLimiter.setRise(params[PARAM_FADE].getValue());
-		slewLimiter.setShape(params[PARAM_SHAPE].getValue());
+		float fade = inputs[INPUT_FADE].getVoltage() / 10.f + params[PARAM_FADE].getValue();
+		slewLimiter.setRise(fade);
+		float shape = inputs[INPUT_SHAPE].getVoltage() / 10.f + params[PARAM_SHAPE].getValue();
+		slewLimiter.setShape(shape);
 		float s = slewLimiter.process(1.f, sampleTime);
 		if (s == 1.f) return;
 
 		for (size_t i = 0; i < sourceHandles.size(); i++) {
 			ParamQuantity* pq = getParamQuantity(sourceHandles[i]);
 			if (!pq) continue;
+			if (presetOld.size() <= i) return;
 			float oldValue = presetOld[i];
+			if (presetSlot[preset].size() <= i) return;
 			float newValue = presetSlot[preset][i];
 			float v = oldValue * (1.f - s) + newValue * s;
 			pq->setValue(v);
@@ -357,19 +339,13 @@ struct TransitModule : Module {
 		if (preset >= p) preset = 0;
 		presetCount = p;
 		presetNext = -1;
-		delete randDist;
-		randDist = new std::uniform_int_distribution<int>(0, presetCount - 1);
+		randDist = std::uniform_int_distribution<int>(0, presetCount - 1);
 	}
 
 	json_t* dataToJson() override {
 		json_t* rootJ = json_object();
 		json_object_set_new(rootJ, "panelTheme", json_integer(panelTheme));
 		json_object_set_new(rootJ, "mappingIndicatorHidden", json_boolean(mappingIndicatorHidden));
-
-		json_object_set_new(rootJ, "sourcePluginSlug", json_string(sourcePluginSlug.c_str()));
-		json_object_set_new(rootJ, "sourcePluginName", json_string(sourcePluginName.c_str()));
-		json_object_set_new(rootJ, "sourceModelSlug", json_string(sourceModelSlug.c_str()));
-		json_object_set_new(rootJ, "sourceModelName", json_string(sourceModelName.c_str()));
 
 		json_object_set_new(rootJ, "slotCvMode", json_integer(slotCvMode));
 		json_object_set_new(rootJ, "preset", json_integer(preset));
@@ -404,24 +380,15 @@ struct TransitModule : Module {
 	}
 
 	void dataFromJson(json_t* rootJ) override {
-		// Hack for preventing duplicating this module
-		if (APP->engine->getModule(id) != NULL) return;
-
 		panelTheme = json_integer_value(json_object_get(rootJ, "panelTheme"));
 		mappingIndicatorHidden = json_boolean_value(json_object_get(rootJ, "mappingIndicatorHidden"));
-
-		json_t* sourcePluginSlugJ = json_object_get(rootJ, "sourcePluginSlug");
-		if (sourcePluginSlugJ) sourcePluginSlug = json_string_value(sourcePluginSlugJ);
-		json_t* sourcePluginNameJ = json_object_get(rootJ, "sourcePluginName");
-		if (sourcePluginNameJ) sourcePluginName = json_string_value(sourcePluginNameJ);
-		json_t* sourceModelSlugJ = json_object_get(rootJ, "sourceModelSlug");
-		if (sourceModelSlugJ) sourceModelSlug = json_string_value(sourceModelSlugJ);
-		json_t* sourceModelNameJ = json_object_get(rootJ, "sourceModelName");
-		if (sourceModelNameJ) sourceModelName = json_string_value(sourceModelNameJ);
 
 		slotCvMode = (SLOTCVMODE)json_integer_value(json_object_get(rootJ, "slotCvMode"));
 		preset = json_integer_value(json_object_get(rootJ, "preset"));
 		presetCount = json_integer_value(json_object_get(rootJ, "presetCount"));
+
+		// Hack for preventing duplicating this module
+		if (APP->engine->getModule(id) != NULL) return;
 
 		inChange = true;
 		json_t* sourceMapsJ = json_object_get(rootJ, "sourceMaps");
@@ -464,8 +431,8 @@ struct TransitModule : Module {
 	}
 };
 
-struct TransitWidget : ThemedModuleWidget<TransitModule<8>> {
-	typedef TransitModule<8> MODULE;
+struct TransitWidget : ThemedModuleWidget<TransitModule<16>> {
+	typedef TransitModule<16> MODULE;
 
 	TransitWidget(MODULE* module)
 		: ThemedModuleWidget<MODULE>(module, "Transit") {
@@ -474,49 +441,28 @@ struct TransitWidget : ThemedModuleWidget<TransitModule<8>> {
 		addChild(createWidget<StoermelderBlackScrew>(Vec(RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<StoermelderBlackScrew>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addInput(createInputCentered<StoermelderPort>(Vec(22.5f, 59.8f), module, MODULE::SLOT_INPUT));
-		//addInput(createInputCentered<StoermelderPort>(Vec(22.5f, 95.2f), module, MODULE::RESET_INPUT));
+		for (size_t i = 0; i < 16; i++) {
+			float o = i * 19.247f;
+			addParam(createParamCentered<LEDButton>(Vec(17.1f, 45.4f + o), module, MODULE::PARAM_PRESET + i));
+			addChild(createLightCentered<LargeLight<RedGreenBlueLight>>(Vec(17.1f, 45.4f + o), module, MODULE::LIGHT_PRESET + i * 3));
+		}
 
-		addParam(createParamCentered<StoermelderTrimpot>(Vec(22.5f, 88.0f), module, MODULE::PARAM_FADE));
-		addParam(createParamCentered<StoermelderTrimpot>(Vec(22.5f, 115.6f), module, MODULE::PARAM_SHAPE));
+		addInput(createInputCentered<StoermelderPort>(Vec(52.6f, 58.9f), module, MODULE::INPUT_SLOT));
+		addInput(createInputCentered<StoermelderPort>(Vec(52.6f, 95.2f), module, MODULE::INPUT_RESET));
 
-		addParam(createParamCentered<LEDButton>(Vec(22.5f, 140.6f), module, MODULE::PRESET_PARAM + 0));
-		addParam(createParamCentered<LEDButton>(Vec(22.5f, 164.1f), module, MODULE::PRESET_PARAM + 1));
-		addParam(createParamCentered<LEDButton>(Vec(22.5f, 187.7f), module, MODULE::PRESET_PARAM + 2));
-		addParam(createParamCentered<LEDButton>(Vec(22.5f, 211.2f), module, MODULE::PRESET_PARAM + 3));
-		addParam(createParamCentered<LEDButton>(Vec(22.5f, 234.8f), module, MODULE::PRESET_PARAM + 4));
-		addParam(createParamCentered<LEDButton>(Vec(22.5f, 258.3f), module, MODULE::PRESET_PARAM + 5));
-		addParam(createParamCentered<LEDButton>(Vec(22.5f, 281.9f), module, MODULE::PRESET_PARAM + 6));
-		addParam(createParamCentered<LEDButton>(Vec(22.5f, 305.4f), module, MODULE::PRESET_PARAM + 7));
+		addParam(createParamCentered<LEDSliderBlue>(Vec(52.6f, 170.2f), module, MODULE::PARAM_FADE));
+		addInput(createInputCentered<StoermelderPort>(Vec(52.6f, 225.9f), module, MODULE::INPUT_FADE));
 
-		addChild(createLightCentered<LargeLight<RedGreenBlueLight>>(Vec(22.5f, 140.6f), module, MODULE::PRESET_LIGHT + 0 * 3));
-		addChild(createLightCentered<LargeLight<RedGreenBlueLight>>(Vec(22.5f, 164.1f), module, MODULE::PRESET_LIGHT + 1 * 3));
-		addChild(createLightCentered<LargeLight<RedGreenBlueLight>>(Vec(22.5f, 187.7f), module, MODULE::PRESET_LIGHT + 2 * 3));
-		addChild(createLightCentered<LargeLight<RedGreenBlueLight>>(Vec(22.5f, 211.2f), module, MODULE::PRESET_LIGHT + 3 * 3));
-		addChild(createLightCentered<LargeLight<RedGreenBlueLight>>(Vec(22.5f, 234.8f), module, MODULE::PRESET_LIGHT + 4 * 3));
-		addChild(createLightCentered<LargeLight<RedGreenBlueLight>>(Vec(22.5f, 258.3f), module, MODULE::PRESET_LIGHT + 5 * 3));
-		addChild(createLightCentered<LargeLight<RedGreenBlueLight>>(Vec(22.5f, 281.9f), module, MODULE::PRESET_LIGHT + 6 * 3));
-		addChild(createLightCentered<LargeLight<RedGreenBlueLight>>(Vec(22.5f, 305.4f), module, MODULE::PRESET_LIGHT + 7 * 3));
+		addParam(createParamCentered<StoermelderTrimpot>(Vec(52.6f, 270.6f), module, MODULE::PARAM_SHAPE));
+		addInput(createInputCentered<StoermelderPort>(Vec(52.6f, 295.6f), module, MODULE::INPUT_SHAPE));
 
-		addParam(createParamCentered<CKSSH>(Vec(22.5f, 336.2f), module, MODULE::MODE_PARAM));
+		addParam(createParamCentered<CKSSH>(Vec(52.6f, 336.2f), module, MODULE::PARAM_RW));
 	}
 
 	void appendContextMenu(Menu* menu) override {
 		ThemedModuleWidget<MODULE>::appendContextMenu(menu);
 		MODULE* module = dynamic_cast<MODULE*>(this->module);
 		assert(module);
-
-		if (module->sourceModelSlug != "") {
-			menu->addChild(new MenuSeparator());
-
-			ui::MenuLabel* textLabel = new ui::MenuLabel;
-			textLabel->text = "Configured for...";
-			menu->addChild(textLabel);
-
-			ui::MenuLabel* modelLabel = new ui::MenuLabel;
-			modelLabel->text = module->sourcePluginName + " " + module->sourceModelName;
-			menu->addChild(modelLabel);
-		}
 
 		struct MappingIndicatorHiddenItem : MenuItem {
 			MODULE* module;
@@ -526,13 +472,6 @@ struct TransitWidget : ThemedModuleWidget<TransitModule<8>> {
 			void step() override {
 				rightText = module->mappingIndicatorHidden ? "✔" : "";
 				MenuItem::step();
-			}
-		};
-
-		struct BindSourceItem : MenuItem {
-			MODULE* module;
-			void onAction(const event::Action& e) override {
-				module->bindToSource();
 			}
 		};
 
@@ -569,15 +508,23 @@ struct TransitWidget : ThemedModuleWidget<TransitModule<8>> {
 			}
 		};
 
-		menu->addChild(construct<BindSourceItem>(&MenuItem::text, "Bind module (left)", &BindSourceItem::module, module));
+		struct BindModuleItem : MenuItem {
+			MODULE* module;
+			void onAction(const event::Action& e) override {
+				module->bindModule();
+			}
+		};
+
 		menu->addChild(new MenuSeparator());
 		menu->addChild(construct<MappingIndicatorHiddenItem>(&MenuItem::text, "Hide mapping indicators", &MappingIndicatorHiddenItem::module, module));
 		menu->addChild(new MenuSeparator());
 		menu->addChild(construct<SlovCvModeMenuItem>(&MenuItem::text, "Port SLOT mode", &SlovCvModeMenuItem::module, module));
+		menu->addChild(new MenuSeparator());
+		menu->addChild(construct<BindModuleItem>(&MenuItem::text, "Bind module (left)", &BindModuleItem::module, module));
 	}
 };
 
 } // namespace Transit
 } // namespace StoermelderPackOne
 
-Model* modelTransit = createModel<StoermelderPackOne::Transit::TransitModule<8>, StoermelderPackOne::Transit::TransitWidget>("Transit");
+Model* modelTransit = createModel<StoermelderPackOne::Transit::TransitModule<16>, StoermelderPackOne::Transit::TransitWidget>("Transit");
