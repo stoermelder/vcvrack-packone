f813e88b13fa680552be64274e5d50ebf88bf7e9
stoermelder <ben@kristall-klang.de>
2020-08-30 00:07:57 +0200

    Midi-Cat - moved scaling/slewing to separate file

---
+++ src/digital/ScaledMapParam.hpp
@@ -0,0 +1,100 @@
+#include "plugin.hpp"
+
+namespace StoermelderPackOne {
+
+template<typename T, T const &UNINIT>
+struct ScaledMapParam {
+	ParamQuantity* paramQuantity;
+	T absoluteMin;
+	T absoluteMax;
+	float min = 0.f;
+	float max = 1.f;
+
+	dsp::ExponentialSlewLimiter filter;
+	bool filterInitialized;
+	float filterSlew;
+	T valueIn;
+	float value;
+	float valueOut;
+
+	ScaledMapParam() {
+		reset();
+	}
+
+	void setAbsolutes(T min, T max) {
+		absoluteMin = min;
+		absoluteMax = max;
+	}
+
+	void reset() {
+		filter.reset();
+		filterInitialized = false;
+		filterSlew = 0.f;
+		valueIn = UNINIT;
+		value = -1.f;
+		valueOut = std::numeric_limits<float>::infinity();
+		min = 0.f;
+		max = 1.f;
+	}
+
+	void setSlew(float slew) {
+		filterSlew = slew;
+		float s = (1.f / slew) * 10.f;
+		filter.setRiseFall(s, s);
+	}
+	float getSlew() {
+		return filterSlew;
+	}
+
+	void setMin(float v) {
+		min = v;
+		if (valueIn != -1) setValue(valueIn);
+	}
+	float getMin() {
+		return min;
+	}
+
+	void setMax(float v) {
+		max = v;
+		if (valueIn != -1) setValue(valueIn);
+	}
+	float getMax() {
+		return max;
+	}
+
+	void setValue(T i) {
+		float f = rescale(float(i), float(absoluteMin), float(absoluteMax), min, max);
+		f = clamp(f, 0.f, 1.f);
+		f = rescale(f, 0.f, 1.f, paramQuantity->getMinValue(), paramQuantity->getMaxValue());
+		valueIn = i;
+		value = f;
+	}
+
+	void process(float sampleTime = -1.f) {
+		if (valueOut == std::numeric_limits<float>::infinity()) return;
+		// Set filter from param value if filter is uninitialized
+		if (!filterInitialized) {
+			filter.out = value = paramQuantity->getValue();
+			filterInitialized = true;
+		}
+		float f = filterSlew > 0.f && sampleTime > 0.f ? filter.process(sampleTime, value) : value;
+		if (valueOut != f) {
+			paramQuantity->setValue(f);
+			valueOut = f;
+		}
+	}
+
+	T getValue() {
+		float f = paramQuantity->getValue();
+		if (isNear(valueOut, f)) return valueIn;
+		if (valueOut == std::numeric_limits<float>::infinity()) value = valueOut = f;
+		f = rescale(f, paramQuantity->getMinValue(), paramQuantity->getMaxValue(), 0.f, 1.f);
+		f = rescale(f, min, max, float(absoluteMin), float(absoluteMax));
+		f = clamp(f, 0.f, 127.f);
+		int i = T(f);
+		if (valueIn == UNINIT) valueIn = i;
+		return i;
+	}
+}; // struct ScaledMapParam
+
+} // namespace StoermelderPackOne
