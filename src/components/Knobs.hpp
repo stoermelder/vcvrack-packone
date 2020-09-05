8e5132ae48f2d9354686b2ce8a7289daedf186fc
stoermelder <ben@kristall-klang.de>
2020-09-05 10:39:54 +0200

    LargeKnob refinements

--- src/components/Knobs.hpp
+++ src/components/Knobs.hpp
@@ -7,6 +7,7 @@ struct StoermelderLargeKnob : app::SvgKnob {
 	StoermelderLargeKnob() {
 		minAngle = -0.83 * M_PI;
 		maxAngle = 0.83 * M_PI;
+		shadow->blurRadius = 1.5f;
 		setSvg(APP->window->loadSvg(asset::plugin(pluginInstance, "res/components/LargeKnob.svg")));
 		sw->box.size = shadow->box.size = Vec(36.0f, 36.0f);
 	}
