af9fcbcfc9eefec7b43b6e64052c30a81bc45762
stoermelder <ben@kristall-klang.de>
2020-11-08 16:25:47 +0100

    Midi-Mon - wip

--- src/MidiMon.cpp
+++ src/MidiMon.cpp
@@ -5,7 +5,7 @@
 namespace StoermelderPackOne {
 namespace MidiMon {
 
-const int LINEBUFFER = 200;
+const int BUFFERSIZE = 800;
 
 struct MidiMonModule : Module {
 	enum ParamIds {
@@ -39,7 +39,7 @@ struct MidiMonModule : Module {
 	/** [Stored to JSON] */
 	midi::InputQueue midiInput;
 
-	dsp::RingBuffer<std::string, 512> midiLogMessages;
+	dsp::RingBuffer<std::tuple<float, std::string>, 512> midiLogMessages;
 	int sample;
 
 	MidiMonModule() {
@@ -55,11 +55,20 @@ struct MidiMonModule : Module {
 		showPitchWheelMsg = true;
 		showChannelPressurelMsg = true;
 		showCcMsg = false;
-		sample = 0;
+		resetTimestamp();
 		Module::onReset();
 	}
 
 	void onSampleRateChange() override {
+		resetTimestamp();
+	}
+
+	void resetTimestamp() {
+		std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+		char buf[100] = {0};
+		std::strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", std::localtime(&now));
+		midiLogMessages.push(std::make_tuple(0.f, std::string(buf)));
+		midiLogMessages.push(std::make_tuple(0.f, string::f("sample rate %i", int(APP->engine->getSampleRate()))));
 		sample = 0;
 	}
 
@@ -79,8 +88,8 @@ struct MidiMonModule : Module {
 					uint8_t ch = msg.getChannel();
 					uint8_t note = msg.getNote();
 					uint8_t vel = msg.getValue();
-					std::string s = string::f("[%8.3f] ch%i note %i vel %i", timestamp, ch + 1, note, vel);
-					midiLogMessages.push(s);
+					std::string s = string::f("ch%i note %i vel %i", ch + 1, note, vel);
+					midiLogMessages.push(std::make_tuple(timestamp, s));
 				}
 				break;
 			case 0x8: // note off
@@ -88,8 +97,8 @@ struct MidiMonModule : Module {
 					uint8_t ch = msg.getChannel();
 					uint8_t note = msg.getNote();
 					uint8_t vel = msg.getValue();
-					std::string s = string::f("[%8.3f] ch%i note off %i vel %i", timestamp, ch + 1, note, vel);
-					midiLogMessages.push(s);
+					std::string s = string::f("ch%i note off %i vel %i", ch + 1, note, vel);
+					midiLogMessages.push(std::make_tuple(timestamp, s));
 				}
 				break;
 			case 0xa: // key pressure
@@ -97,24 +106,24 @@ struct MidiMonModule : Module {
 					uint8_t ch = msg.getChannel();
 					uint8_t note = msg.getNote();
 					uint8_t value = msg.getValue();
-					std::string s = string::f("[%8.3f] ch%i key-press %i vel %i", timestamp, ch + 1, note, value);
-					midiLogMessages.push(s);
+					std::string s = string::f("ch%i key-pressure %i vel %i", ch + 1, note, value);
+					midiLogMessages.push(std::make_tuple(timestamp, s));
 				}
 				break;
 			case 0xd: // channel pressure
 				if (!midiLogMessages.full() && showChannelPressurelMsg) {
 					uint8_t ch = msg.getChannel();
 					uint8_t value = msg.getNote();
-					std::string s = string::f("[%8.3f] ch%i chan-pressure=%i", timestamp, ch + 1, value);
-					midiLogMessages.push(s);
+					std::string s = string::f("ch%i channel-pressure=%i", ch + 1, value);
+					midiLogMessages.push(std::make_tuple(timestamp, s));
 				}
 				break;
 			case 0xe: // pitch wheel
 				if (!midiLogMessages.full() && showPitchWheelMsg) {
 					uint8_t ch = msg.getChannel();
 					uint16_t value = ((uint16_t)msg.getValue() << 7) | msg.getNote();
-					std::string s = string::f("[%8.3f] ch%i pitchwheel=%i", timestamp, ch + 1, value);
-					midiLogMessages.push(s);
+					std::string s = string::f("ch%i pitchwheel=%i", ch + 1, value);
+					midiLogMessages.push(std::make_tuple(timestamp, s));
 				} 
 				break;
 			case 0xb: // cc
@@ -122,24 +131,24 @@ struct MidiMonModule : Module {
 					uint8_t ch = msg.getChannel();
 					uint8_t cc = msg.getNote();
 					int8_t value = msg.bytes[2];
-					std::string s = string::f("[%8.3f] ch%i cc%i=%i", timestamp, ch + 1, cc, value);
-					midiLogMessages.push(s);
+					std::string s = string::f("ch%i cc%i=%i", ch + 1, cc, value);
+					midiLogMessages.push(std::make_tuple(timestamp, s));
 				}
 				break;
 			case 0xf: // system
 				if (!midiLogMessages.full()) {
 					switch (msg.getChannel()) {
 						case 0x8: // clock
-							if (showClockMsg) midiLogMessages.push(string::f("[%8.3f] clock tick", timestamp));
+							if (showClockMsg) midiLogMessages.push(std::make_tuple(timestamp, "clock tick"));
 							break;
 						case 0xa: // start
-							if (showSystemMsg) midiLogMessages.push(string::f("[%8.3f] start", timestamp));
+							if (showSystemMsg) midiLogMessages.push(std::make_tuple(timestamp, "start"));
 							break;
 						case 0xb: // continue
-							if (showSystemMsg) midiLogMessages.push(string::f("[%8.3f] continue", timestamp));
+							if (showSystemMsg) midiLogMessages.push(std::make_tuple(timestamp, "continue"));
 							break;
 						case 0xc: // stop
-							if (showSystemMsg) midiLogMessages.push(string::f("[%8.3f] stop", timestamp));
+							if (showSystemMsg) midiLogMessages.push(std::make_tuple(timestamp, "stop"));
 							break;
 						default:
 							break;
@@ -167,6 +176,8 @@ struct MidiMonModule : Module {
 	}
 
 	void dataFromJson(json_t* rootJ) override {
+		panelTheme = json_integer_value(json_object_get(rootJ, "panelTheme"));
+
 		showClockMsg = json_boolean_value(json_object_get(rootJ, "showClockMsg"));
 		showSystemMsg = json_boolean_value(json_object_get(rootJ, "showSystemMsg"));
 		showNoteMsg = json_boolean_value(json_object_get(rootJ, "showNoteMsg"));
@@ -182,7 +193,7 @@ struct MidiMonModule : Module {
 
 struct MidiDisplay : LedTextDisplay {
 	MidiMonModule* module;
-	std::list<std::string>* buffer;
+	std::list<std::tuple<float, std::string>>* buffer;
 	bool dirty = true;
 
 	MidiDisplay() {
@@ -193,11 +204,11 @@ struct MidiDisplay : LedTextDisplay {
 		LedTextDisplay::step();
 		if (dirty) {
 			text = "";
-			size_t size = std::min(buffer->size(), (size_t)19);
+			size_t size = std::min(buffer->size(), (size_t)(box.size.x / fontSize - 1));
 			size_t i = 0;
-			for (std::string s : *buffer) {
+			for (std::tuple<float, std::string> s : *buffer) {
 				if (i >= size) break;
-				text += s + "\n";
+				text += string::f("[%7.2f] %s\n", std::get<0>(s), std::get<1>(s).c_str());
 				i++;
 			}
 		}
@@ -230,7 +241,7 @@ struct MidiMonMidiWidget : MidiWidget {
 
 struct MidiMonWidget : ThemedModuleWidget<MidiMonModule> {
 	MidiDisplay* textField;
-	std::list<std::string> buffer;
+	std::list<std::tuple<float, std::string>> buffer;
 	
 	MidiMonWidget(MidiMonModule* module)
 		: ThemedModuleWidget<MidiMonModule>(module, "MidiMon") {
@@ -258,8 +269,8 @@ struct MidiMonWidget : ThemedModuleWidget<MidiMonModule> {
 		if (!module) return;
 		MidiMonModule* module = reinterpret_cast<MidiMonModule*>(this->module);
 		while (!module->midiLogMessages.empty()) {
-			if (buffer.size() == LINEBUFFER) buffer.pop_back();
-			std::string s = module->midiLogMessages.shift();
+			if (buffer.size() == BUFFERSIZE) buffer.pop_back();
+			std::tuple<float, std::string> s = module->midiLogMessages.shift();
 			buffer.push_front(s);
 			textField->dirty = true;
 		}
@@ -292,7 +303,7 @@ struct MidiMonWidget : ThemedModuleWidget<MidiMonModule> {
 		struct ExportItem : MenuItem {
 			MidiMonWidget* mw;
 			void onAction(const event::Action& e) override {
-				mw->saveFileDialog();
+				mw->exportLogDialog();
 			}
 		};
 
@@ -300,7 +311,7 @@ struct MidiMonWidget : ThemedModuleWidget<MidiMonModule> {
 		menu->addChild(construct<ExportItem>(&MenuItem::text, "Export log", &ExportItem::mw, this));
 	}
 
-	void saveFile(std::string filename) {
+	void exportLog(std::string filename) {
 		INFO("Saving file %s", filename.c_str());
 
 		FILE* file = fopen(filename.c_str(), "w");
@@ -312,15 +323,31 @@ struct MidiMonWidget : ThemedModuleWidget<MidiMonModule> {
 			fclose(file);
 		});
 
-		for (std::list<std::string>::reverse_iterator rit = buffer.rbegin(); rit != buffer.rend(); rit++) {
-			std::string s = *rit;
-			fputs(s.append("\n").c_str(), file);
+		fputs(string::f("%s v%s\n", app::APP_NAME.c_str(), app::APP_VERSION.c_str()).c_str(), file);
+		fputs(string::f("%s\n", system::getOperatingSystemInfo().c_str()).c_str(), file);
+		fputs(string::f("MIDI driver: %s\n", module->midiInput.getDriverName(module->midiInput.driverId).c_str()).c_str(), file);
+		fputs(string::f("MIDI device: %s\n", module->midiInput.getDeviceName(module->midiInput.deviceId).c_str()).c_str(), file);
+		fputs(string::f("MIDI channel: %s\n", module->midiInput.getChannelName(module->midiInput.channel).c_str()).c_str(), file);
+		fputs("--------------------------------------------------------------------\n", file);
+
+		for (std::list<std::tuple<float, std::string>>::reverse_iterator rit = buffer.rbegin(); rit != buffer.rend(); rit++) {
+			std::tuple<float, std::string> s = *rit;
+			fputs(string::f("[%11.4f] %s\n", std::get<0>(s), std::get<1>(s).c_str()).c_str(), file);
 		}
 	}
 
-	void saveFileDialog() {
-		std::string dir = asset::user("patches");
-		char* path = osdialog_file(OSDIALOG_SAVE, NULL, "Midi.log", NULL);
+	void exportLogDialog() {
+		static const char PRESET_FILTERS[] = "*:*";
+		osdialog_filters* filters = osdialog_filters_parse(PRESET_FILTERS);
+		DEFER({
+			osdialog_filters_free(filters);
+		});
+
+		std::string log = asset::user("MidiMon.log");
+		std::string dir = string::directory(log);
+		std::string filename = string::filename(log);
+
+		char* path = osdialog_file(OSDIALOG_SAVE, dir.c_str(), NULL, filters);
 		if (!path) {
 			// No path selected
 			return;
@@ -330,7 +357,7 @@ struct MidiMonWidget : ThemedModuleWidget<MidiMonModule> {
 		});
 
 		std::string pathStr = path;
-		saveFile(pathStr);
+		exportLog(pathStr);
 	}
 };
 
