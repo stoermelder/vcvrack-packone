d36a5a7ba5d95d60a518da2de897bc3c0d3e4900
stoermelder <ben@kristall-klang.de>
2020-11-08 11:13:54 +0100

    Midi-Mon - wip

---
+++ docs/MidiMon.md
@@ -0,0 +1,5 @@
+# stoermelder MIDI-MON
+
+
+
+MIDI-MON was added in v1.8.0 of PackOne.
--- src/MidiMon.cpp
+++ src/MidiMon.cpp
@@ -1,7 +1,11 @@
 #include "plugin.hpp"
+#include "components/LedTextDisplay.hpp"
+#include <osdialog.h>
 
 namespace StoermelderPackOne {
 namespace MidiMon {
+
+const int LINEBUFFER = 200;
 
 struct MidiMonModule : Module {
 	enum ParamIds {
@@ -36,6 +40,7 @@ struct MidiMonModule : Module {
 	midi::InputQueue midiInput;
 
 	dsp::RingBuffer<std::string, 512> midiLogMessages;
+	int sample;
 
 	MidiMonModule() {
 		panelTheme = pluginSettings.panelThemeDefault;
@@ -50,7 +55,12 @@ struct MidiMonModule : Module {
 		showPitchWheelMsg = true;
 		showChannelPressurelMsg = true;
 		showCcMsg = false;
+		sample = 0;
 		Module::onReset();
+	}
+
+	void onSampleRateChange() override {
+		sample = 0;
 	}
 
 	void process(const ProcessArgs& args) override {
@@ -58,16 +68,18 @@ struct MidiMonModule : Module {
 		while (midiInput.shift(&msg)) {
 			processMidi(msg);
 		}
+		sample++;
 	}
 
 	void processMidi(midi::Message& msg) {
+		float timestamp = float(sample) / APP->engine->getSampleRate();
 		switch (msg.getStatus()) {
 			case 0x9: // note on
 				if (!midiLogMessages.full() && showNoteMsg) {
 					uint8_t ch = msg.getChannel();
 					uint8_t note = msg.getNote();
 					uint8_t vel = msg.getValue();
-					std::string s = string::f("ch %i note %i vel %i", ch + 1, note, vel);
+					std::string s = string::f("[%8.3f] ch%i note %i vel %i", timestamp, ch + 1, note, vel);
 					midiLogMessages.push(s);
 				}
 				break;
@@ -76,7 +88,7 @@ struct MidiMonModule : Module {
 					uint8_t ch = msg.getChannel();
 					uint8_t note = msg.getNote();
 					uint8_t vel = msg.getValue();
-					std::string s = string::f("ch %i note off %i vel %i", ch + 1, note, vel);
+					std::string s = string::f("[%8.3f] ch%i note off %i vel %i", timestamp, ch + 1, note, vel);
 					midiLogMessages.push(s);
 				}
 				break;
@@ -85,7 +97,7 @@ struct MidiMonModule : Module {
 					uint8_t ch = msg.getChannel();
 					uint8_t note = msg.getNote();
 					uint8_t value = msg.getValue();
-					std::string s = string::f("ch %i key pressure %i vel %i", ch + 1, note, value);
+					std::string s = string::f("[%8.3f] ch%i key-press %i vel %i", timestamp, ch + 1, note, value);
 					midiLogMessages.push(s);
 				}
 				break;
@@ -93,7 +105,7 @@ struct MidiMonModule : Module {
 				if (!midiLogMessages.full() && showChannelPressurelMsg) {
 					uint8_t ch = msg.getChannel();
 					uint8_t value = msg.getNote();
-					std::string s = string::f("ch %i channel pressure val %i", ch + 1, value);
+					std::string s = string::f("[%8.3f] ch%i chan-pressure=%i", timestamp, ch + 1, value);
 					midiLogMessages.push(s);
 				}
 				break;
@@ -101,7 +113,7 @@ struct MidiMonModule : Module {
 				if (!midiLogMessages.full() && showPitchWheelMsg) {
 					uint8_t ch = msg.getChannel();
 					uint16_t value = ((uint16_t)msg.getValue() << 7) | msg.getNote();
-					std::string s = string::f("ch %i pitchwheel val %i", ch + 1, value);
+					std::string s = string::f("[%8.3f] ch%i pitchwheel=%i", timestamp, ch + 1, value);
 					midiLogMessages.push(s);
 				} 
 				break;
@@ -110,7 +122,7 @@ struct MidiMonModule : Module {
 					uint8_t ch = msg.getChannel();
 					uint8_t cc = msg.getNote();
 					int8_t value = msg.bytes[2];
-					std::string s = string::f("ch %i cc %i val %i", ch + 1, cc, value);
+					std::string s = string::f("[%8.3f] ch%i cc%i=%i", timestamp, ch + 1, cc, value);
 					midiLogMessages.push(s);
 				}
 				break;
@@ -118,16 +130,16 @@ struct MidiMonModule : Module {
 				if (!midiLogMessages.full()) {
 					switch (msg.getChannel()) {
 						case 0x8: // clock
-							if (showClockMsg) midiLogMessages.push("clock tick");
+							if (showClockMsg) midiLogMessages.push(string::f("[%8.3f] clock tick", timestamp));
 							break;
 						case 0xa: // start
-							if (showSystemMsg) midiLogMessages.push("start");
+							if (showSystemMsg) midiLogMessages.push(string::f("[%8.3f] start", timestamp));
 							break;
 						case 0xb: // continue
-							if (showSystemMsg) midiLogMessages.push("continue");
+							if (showSystemMsg) midiLogMessages.push(string::f("[%8.3f] continue", timestamp));
 							break;
 						case 0xc: // stop
-							if (showSystemMsg) midiLogMessages.push("stop");
+							if (showSystemMsg) midiLogMessages.push(string::f("[%8.3f] stop", timestamp));
 							break;
 						default:
 							break;
@@ -168,18 +180,26 @@ struct MidiMonModule : Module {
 };
 
 
-struct MidiDisplay : LedDisplayTextField {
+struct MidiDisplay : LedTextDisplay {
 	MidiMonModule* module;
-	const int MAX = 600;
+	std::list<std::string>* buffer;
+	bool dirty = true;
+
 	MidiDisplay() {
 		color = nvgRGB(0xf0, 0xf0, 0xf0);
+		fontSize = 10.f;
 	}
 	void step() override {
-		LedDisplayTextField::step();
-		if (!module) return;
-		while (!module->midiLogMessages.empty()) {
-			std::string s = module->midiLogMessages.shift();
-			text = s + "\n" + text.substr(0, MAX);
+		LedTextDisplay::step();
+		if (dirty) {
+			text = "";
+			size_t size = std::min(buffer->size(), (size_t)19);
+			size_t i = 0;
+			for (std::string s : *buffer) {
+				if (i >= size) break;
+				text += s + "\n";
+				i++;
+			}
 		}
 	}
 };
@@ -209,6 +229,9 @@ struct MidiMonMidiWidget : MidiWidget {
 };
 
 struct MidiMonWidget : ThemedModuleWidget<MidiMonModule> {
+	MidiDisplay* textField;
+	std::list<std::string> buffer;
+	
 	MidiMonWidget(MidiMonModule* module)
 		: ThemedModuleWidget<MidiMonModule>(module, "MidiMon") {
 		setModule(module);
@@ -223,11 +246,23 @@ struct MidiMonWidget : ThemedModuleWidget<MidiMonModule> {
 		midiInputWidget->setMidiPort(module ? &module->midiInput : NULL);
 		addChild(midiInputWidget);
 
-		MidiDisplay* textField1 = createWidget<MidiDisplay>(Vec(10.f, 108.7f));
-		textField1->module = module;
-		textField1->box.size = Vec(219.9f, 234.1f);
-		textField1->multiline = true;
-		addChild(textField1);
+		textField = createWidget<MidiDisplay>(Vec(10.f, 108.7f));
+		textField->module = module;
+		textField->buffer = &buffer;
+		textField->box.size = Vec(219.9f, 234.1f);
+		addChild(textField);
+	}
+
+	void step() override {
+		ThemedModuleWidget<MidiMonModule>::step();
+		if (!module) return;
+		MidiMonModule* module = reinterpret_cast<MidiMonModule*>(this->module);
+		while (!module->midiLogMessages.empty()) {
+			if (buffer.size() == LINEBUFFER) buffer.pop_back();
+			std::string s = module->midiLogMessages.shift();
+			buffer.push_front(s);
+			textField->dirty = true;
+		}
 	}
 
 	void appendContextMenu(Menu* menu) override {
@@ -253,6 +288,49 @@ struct MidiMonWidget : ThemedModuleWidget<MidiMonModule> {
 		menu->addChild(construct<MsgItem>(&MenuItem::text, "Pitch wheel", &MsgItem::s, &module->showPitchWheelMsg));
 		menu->addChild(construct<MsgItem>(&MenuItem::text, "Channel pressure", &MsgItem::s, &module->showChannelPressurelMsg));
 		menu->addChild(construct<MsgItem>(&MenuItem::text, "CCs", &MsgItem::s, &module->showCcMsg));
+
+		struct ExportItem : MenuItem {
+			MidiMonWidget* mw;
+			void onAction(const event::Action& e) override {
+				mw->saveFileDialog();
+			}
+		};
+
+		menu->addChild(new MenuSeparator());
+		menu->addChild(construct<ExportItem>(&MenuItem::text, "Export log", &ExportItem::mw, this));
+	}
+
+	void saveFile(std::string filename) {
+		INFO("Saving file %s", filename.c_str());
+
+		FILE* file = fopen(filename.c_str(), "w");
+		if (!file) {
+			std::string message = string::f("Could not write to file %s", filename.c_str());
+			osdialog_message(OSDIALOG_WARNING, OSDIALOG_OK, message.c_str());
+		}
+		DEFER({
+			fclose(file);
+		});
+
+		for (std::list<std::string>::reverse_iterator rit = buffer.rbegin(); rit != buffer.rend(); rit++) {
+			std::string s = *rit;
+			fputs(s.append("\n").c_str(), file);
+		}
+	}
+
+	void saveFileDialog() {
+		std::string dir = asset::user("patches");
+		char* path = osdialog_file(OSDIALOG_SAVE, NULL, "Midi.log", NULL);
+		if (!path) {
+			// No path selected
+			return;
+		}
+		DEFER({
+			free(path);
+		});
+
+		std::string pathStr = path;
+		saveFile(pathStr);
 	}
 };
 
