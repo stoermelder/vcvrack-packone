c81db1c5d435dffeadeac6a2ec775b3393e59718
stoermelder <ben@kristall-klang.de>
2026-01-12 11:44:40 +0100

    Transit - panel refinement

--- src/modules/transit/Transit.cpp
+++ src/modules/transit/Transit.cpp
@@ -1271,11 +1271,11 @@ struct TransitWidget : ThemedModuleWidget<TransitModule<NUM_PRESETS>> {
 		BASE::addInput(createInputCentered<StoermelderPort>(Vec(21.7f, 221.4f), module, MODULE::INPUT_FADE));
 
 		BASE::addParam(createParamCentered<StoermelderTrimpot>(Vec(21.7f, 255.8f), module, MODULE::PARAM_SHAPE));
-		BASE::addOutput(createOutputCentered<StoermelderPort>(Vec(21.7f, 300.3f), module, MODULE::OUTPUT));
+		BASE::addOutput(createOutputCentered<StoermelderPort>(Vec(21.7f, 327.6f), module, MODULE::OUTPUT));
 
-		BASE::addParam(createParamCentered<CKSSThreeH>(Vec(37.5f, 336.2f), module, MODULE::PARAM_CTRLMODE));
+		BASE::addParam(createParamCentered<CKSSThreeH>(Vec(21.7f, 292.0f), module, MODULE::PARAM_CTRLMODE));
 
-		BASE::addChild(createLightCentered<TinyLight<WhiteLight>>(Vec(10.4f, 336.2f), module, MODULE::LIGHT_LEARN));
+		BASE::addChild(createLightCentered<TinyLight<WhiteLight>>(Vec(10.4f, 353.5f), module, MODULE::LIGHT_LEARN));
 
 		for (size_t i = 0; i < NUM_PRESETS; i++) {
 			float o = i * (259.0f / (NUM_PRESETS - 1));
