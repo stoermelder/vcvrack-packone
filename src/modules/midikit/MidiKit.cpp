59b7a19db789d6b747d22cda1a2f1106bc817884
stoermelder <ben@kristall-klang.de>
2026-06-06 13:30:17 +0200

    Midi-Kit - fixed build error

--- src/modules/midikit/MidiKit.cpp
+++ src/modules/midikit/MidiKit.cpp
@@ -3,7 +3,7 @@
 #include "MidiScriptEngineLua.h"
 #include "../../components/Knobs.hpp"
 #include "../../components/MidiWidget.hpp"
-#include "../../components/LogDisplay.hpp"
+#include "../../components/LedTextField.hpp"
 #include "../../ui/OverlayMessageWidget.hpp"
 #include <osdialog.h>
 #include <fstream>
@@ -11,6 +11,14 @@
 
 namespace StoermelderPackOne {
 namespace MidiKit {
+
+
+enum class LOG_FORMAT {
+	RESET,
+	TIMESTAMP,
+	INDENTED,
+	TEXT
+};
 
 struct MidiOutput : midi::Output {
 	struct FrameSchedule {
@@ -431,6 +439,51 @@ struct MidiKitModule : Module {
 
 	void clearScript() {
 		loadScript("");
+	}
+};
+
+
+struct LogDisplay : LedTextDisplay {
+	std::list<std::tuple<LOG_FORMAT, float, std::string>>* buffer;
+	bool dirty = true;
+
+	LogDisplay() {
+		color = nvgRGB(0xf0, 0xf0, 0xf0);
+		bgColor.a = 0.f;
+		fontSize = 9.2f;
+		textOffset.y += 2.f;
+	}
+
+	void step() override {
+		LedTextDisplay::step();
+		if (dirty) {
+			text = "";
+			size_t size = std::min(buffer->size(), (size_t)(box.size.x / fontSize) + 1);
+			size_t i = 0;
+			for (std::tuple<LOG_FORMAT, float, std::string> s : *buffer) {
+				if (i >= size) break;
+				LOG_FORMAT f = std::get<0>(s);
+				float timestamp = std::get<1>(s);
+				switch (f) {
+					case LOG_FORMAT::TIMESTAMP:
+						text += string::f("[%9.4f] %s\n", timestamp, std::get<2>(s).c_str());
+						break;
+					case LOG_FORMAT::TEXT:
+						text += string::f("%s\n", std::get<2>(s).c_str());
+						break;
+					case LOG_FORMAT::INDENTED:
+						text += string::f("     %s\n", std::get<2>(s).c_str());
+						break;
+					default:
+						break;
+				};
+			}
+		}
+	}
+
+	void reset() {
+		buffer->clear();
+		dirty = true;
 	}
 };
 
