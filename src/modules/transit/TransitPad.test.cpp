1418f4f93b5e74e40632e7540d4ed6980993829f
stoermelder <ben@kristall-klang.de>
2026-06-13 07:51:13 +0200

    Transit-Pad - added json test-case

--- src/modules/transit/TransitPad.test.cpp
+++ src/modules/transit/TransitPad.test.cpp
@@ -49,6 +49,20 @@ TEST_CASE("Construction and initialization", "[TransitPad]") {
 }
 
 
+TEST_CASE("Preset JSON null-guards", "[TransitPad][JSON]") {
+	auto module = Test::createModule<TransitPadModule<>>("TransitPad");
+
+	SECTION("All top-level properties are null-guarded in dataFromJson()") {
+		json_t* rootJ = module->dataToJson();
+		REQUIRE(rootJ != nullptr);
+		Test::testPresetNullGuards(module, rootJ);
+		json_decref(rootJ);
+	}
+
+	Test::destroyModule(module);
+}
+
+
 TEST_CASE("SET_PARAM buttons change currentSet", "[TransitPad]") {
 	TransitPadModule<>* m = Test::createModule<TransitPadModule<>>("TransitPad");
 
