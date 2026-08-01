ef3ef32aa5133da0e302a1ceb4211090b0673967
stoermelder <ben@kristall-klang.de>
2026-08-01 14:00:42 +0200

    Midi-Kit - fixed minilua links

--- src/modules/midikit/minilua.c
+++ src/modules/midikit/minilua.c
@@ -1,6 +1,6 @@
 /*
- * Single translation unit that compiles the full Lua 5.4 implementation
- * from minilua (https://github.com/mackron/minilua).
+ * Single translation unit that compiles the full Lua 5.5 implementation
+ * from minilua (https://github.com/edubart/minilua).
  *
  * Only one .c/.cpp file in the project must define LUA_IMPL.
  */
